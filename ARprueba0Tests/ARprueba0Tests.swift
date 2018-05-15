7f68a0a21d0574179db325a22b466b4612cdd48b
jmartinezdejuan <jmartinezdejuan@gmail.com>
2018-05-15 19:40:49 +0200

    Initial Commit

---
+++ ARprueba0Tests/ARprueba0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ARprueba0Tests.swift
+//  ARprueba0Tests
+//
+//  Created by Jesus M Martínez de Juan on 15/5/18.
+//  Copyright © 2018 CHECHU. All rights reserved.
+//
+
+import XCTest
+@testable import ARprueba0
+
+class ARprueba0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
