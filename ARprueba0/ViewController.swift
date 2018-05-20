9aad52af9bdbac00929de1648e836d227af3b0a5
jmartinezdejuan <jmartinezdejuan@gmail.com>
2018-05-20 17:39:45 +0200

    Primer programa de prueba

--- ARprueba0/ViewController.swift
+++ ARprueba0/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 import SceneKit
 import ARKit
 
+extension matrix_float4x4 {
+    func position() -> SCNVector3 {
+        return SCNVector3(columns.3.x, columns.3.y, columns.3.z)
+    }
+}
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
@@ -47,6 +52,44 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {return}
+        let results = sceneView.hitTest(touch.location(in: sceneView), types: [ARHitTestResult.ResultType.featurePoint])
+        guard let hitFeature = results.last else { return }
+        let hitTransform = hitFeature.worldTransform // <- if higher than beta 1, use just this -> hitFeature.worldTransform
+        let position = hitTransform.position()
+        let hitVector = SCNVector3Make(position.x, position.y, position.z)
+        //createBall(position: hitVector)
+        //createBall2()
+        createCuboUFV(position: hitVector)
+    }
+    
+    func createBall(position : SCNVector3){
+        var ballShape = SCNSphere(radius: 0.01)
+        var ballNode = SCNNode(geometry: ballShape)
+        ballNode.position = position
+        sceneView.scene.rootNode.addChildNode(ballNode)
+    }
+    var quepasa = Float(-3.00)
+    func createBall2(){
+        var ballShape = SCNSphere(radius: 0.08)
+        var ballNode = SCNNode(geometry: ballShape)
+        ballNode.position = SCNVector3Make(quepasa,quepasa,quepasa)
+        quepasa = quepasa + 0.5
+        sceneView.scene.rootNode.addChildNode(ballNode)
+        ballShape.firstMaterial?.diffuse.contents = UIColor.brown
+    }
+    func createCuboUFV(position: SCNVector3){
+        var box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "texture.png")
+        box.materials = [material]
+        var boxNode = SCNNode(geometry: box)
+        boxNode.position = position
+        
+        sceneView.scene.rootNode.addChildNode(boxNode)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
